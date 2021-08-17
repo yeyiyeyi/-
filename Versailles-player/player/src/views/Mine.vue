e8c91fc9cc0d05916c4396379c9ed905818404a4
rose55555 <87806709+rose55555@users.noreply.github.com>
2021-08-17 15:41:44 +0800

    Add files via upload

---
+++ Versailles-player/player/src/views/Mine.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-17 14:50:58
+ * @LastEditTime: 2021-08-17 14:51:35
+ * @LastEditors: Please set LastEditors
+ * @Description: 我的
+ * @FilePath: \player\src\views\Mine.vue
+-->
+<template>
+    <div class="mine"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: "Mine",
+    components: {},
+};
+</script>
---
+++ Versailles-player/player/src/views/Podcast.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-17 14:12:16
+ * @LastEditTime: 2021-08-17 14:49:45
+ * @LastEditors: Please set LastEditors
+ * @Description: 播客
+ * @FilePath: \player\src\views\Podcast.vue
+-->
+<template>
+  <div class="Podcast">
+    <h1>This is an about page</h1>
+  </div>
+</template>

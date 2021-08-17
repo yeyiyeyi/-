980a73003cd3a00013846e8568aff428c1a7b3f5
Eacan1 <87850388+Eacan1@users.noreply.github.com>
2021-08-17 16:22:41 +0800

    Add files via upload

---
+++ Player/src/views/CloudVillage.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-17 16:12:06
+ * @LastEditTime: 2021-08-17 16:12:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \player\play\src\views\CloudVillage.vue
+-->
+<template>
+    <div class="cloudVillage"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: "CloudVillage",
+    components: {},
+};
+</script>
---
+++ Player/src/views/SongRoom.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-08-17 16:14:51
+ * @LastEditTime: 2021-08-17 16:14:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \player\play\src\views\SongRoom.vue
+-->
+<template>
+    <div class="songRoom"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: "SongRoom",
+    components: {},
+};
+</script>
